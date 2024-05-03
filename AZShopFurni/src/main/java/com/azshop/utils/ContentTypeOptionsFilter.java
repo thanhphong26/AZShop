27474b21eaccd6609dab286333ee229f553a33c5
Thanh Phong <thanh2652003abc@gmail.com>
2024-05-03 23:24:20 +0700

    AZ-01 | Fix X-Content-Type-Options Header Missing

---
+++ AZShopFurni/src/main/java/com/azshop/utils/ContentTypeOptionsFilter.java
@@ -0,0 +1,30 @@
+package com.azshop.utils;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+@WebFilter("/*")
+public class ContentTypeOptionsFilter implements Filter{
+	@Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("X-Content-Type-Options", "nosniff");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
